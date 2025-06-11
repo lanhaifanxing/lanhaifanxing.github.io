12c44d5bbaa341c32f7c0baf295c76bcf5bd9041
欧文 <1722933849@qq.com>
2025-06-11 21:31:40 +0800

    Site updated: 2025-06-11 21:31:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/11/hello-world/","2025/06/11/first-artice/"];function toRandomPost(){
+var posts=["2023/08/25/first-artice/","2025/06/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
